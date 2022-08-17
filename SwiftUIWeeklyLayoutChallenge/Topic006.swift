92bf3b21a1e4eabc12cbd9bed7c2c2e135e67e2b
SNQ-2001 <taishin05109@gmail.com>
2022-08-17 14:43:46 +0900

    更新: 006

--- SwiftUIWeeklyLayoutChallenge/Topic006.swift
+++ SwiftUIWeeklyLayoutChallenge/Topic006.swift
@@ -28,9 +28,62 @@ public struct Topic006View: View {
 @available(iOS 15.0, *)
 fileprivate struct Topic006ContentView: View {
     private let paddingLength: CGFloat = 16
-    
+    @State var isNewAlert: Bool = false
+    @State var isAddVoiceAlert: Bool = false
+    @State var isAddImageAlert: Bool = false
+    @State var offset: CGSize = .zero
+    @State var isDragging = false
     var body: some View {
-        Text("Code your layout here!")
+        ZStack(alignment: .bottomTrailing) {
+            Text("後ろに位置するView")
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            Menu {
+                Button(action: {
+                    isNewAlert = true
+                }) {
+                    Text("新規作成")
+                    Image(systemName: "square.and.pencil")
+                }
+                Button(action: {
+                    isAddImageAlert = true
+                }) {
+                    Text("画像を追加")
+                    Image(systemName: "photo")
+                }
+                Button(action: {
+                    isAddVoiceAlert = true
+                }) {
+                    Text("音声を追加")
+                    Image(systemName: "mic.badge.plus")
+                }
+            } label: {
+                Image(systemName: "plus")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(.white)
+                    .frame(width: 50, height: 50)
+                    .background(isNewAlert ? Color.gray : isAddVoiceAlert ? Color.gray : isAddImageAlert ? Color.gray : Color.blue)
+                    .clipShape(Circle())
+            } primaryAction: {
+                isNewAlert = true
+            }
+            .offset(offset)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        offset = CGSize(width: value.location.x - 25, height: value.location.y - 25)
+                        isDragging = true
+                    }
+                    .onEnded { _ in
+                        isDragging = false
+                    }
+            )
+            .padding(.all, paddingLength)
+            .alert("新規作成", isPresented: $isNewAlert) { }
+            .alert("音声を追加", isPresented: $isAddVoiceAlert) { }
+            .alert("画像を追加", isPresented: $isAddImageAlert) { }
+        }
     }
 }
 #endif
